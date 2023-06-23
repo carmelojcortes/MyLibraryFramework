e81411cb35452e4479c0402f833401bc03edb0b9
Carmelo J Cortés Alhambra <>
2023-06-23 17:49:50 +0200

    change texts to test if pipeline is able to update the file with the proper info

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyLib",
-            url: "https://github.com/carmelojcortes/MyLibrary/releases/download/release%2Ftest13/MyLib.zip",
-            checksum: "454095c523cf43b86951f9714baf82d0adf8cfba89c08e238852f85cc435b799"
+            url: "to be replaced",
+            checksum: "to be replaced"
         )
     ]
 )
